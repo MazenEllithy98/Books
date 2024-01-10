c97899e09556c73cf517a56cac60bd25874cb567
MazenEllithy98 <mazen.a.ellethy@gmail.com>
2024-01-10 04:44:01 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    //public delegate string BookDelegate(Book B);
+    
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Book(string iSBN, string title, string[] authors, DateTime publicationDate, decimal price)
+        {
+            this.ISBN = iSBN;
+            this.Title = title;
+            this.Authors = authors;
+            this.PublicationDate = publicationDate;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN : {ISBN} , Book Title : {Title} , Book Authors {Authors} , Publication Date : {PublicationDate} , Book Price : {Price}";
+        }
+
+
+    }
+
+
+    public class BookFunctions 
+        {
+            public static string GetTitle (Book B )
+        { return B.Title; }
+
+        public static string GetAuthor(Book B)
+        {
+           return B.Authors[0];
+        }
+        public static string GetPrice(Book B)
+        { return B.Price.ToString(); }
+
+        }
+
+
+    
+    public class LibraryEngine
+    {
+        /// <summary>
+        /// Done user defined delegate to parametrize the BookFunctions Methods.
+        /// Then Used the suitable built in delegate (predicate)
+        /// </summary>
+        /// 
+        //public static void ProcessBooks(List<Book> bList, BookDelegate bookdelegate)
+        //{
+        //    foreach (Book b in bList)
+        //       Console.WriteLine(bookdelegate(b));
+
+        //}
+
+
+        public static void ProcessBooks(List<Book> bList, Func<Book , string> func)
+        {
+            foreach (Book B in bList)
+                Console.WriteLine(func(B));
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Book01", "The First Book", new string[1] { "Mazen Ellithy" }, new DateTime(2009, 1, 16), 32.5M);
+
+            Book book2 = new Book("Book02", "The Second Book", new string[1] { "Hamada" }, new DateTime(2010, 2, 11), 35.5M);
+
+            Book book3 = new Book("Book03", "The Third Book", new string[1] { "Mohamed Lotfy" }, new DateTime(2020, 10, 30), 40.5M);
+
+            List<Book> books= new List<Book>()
+            {
+                book1,
+                book2,
+                book3
+            };
+
+
+
+            // GetISBN as Anonymous Method
+            Func<Book, string> func = delegate (Book book) { return book.ISBN; };
+            Console.WriteLine($"ISBN for Book1 is : {func(book1)} ");
+
+            // Get Publication date as anonymous method with Lambda Expression
+            Func<Book, DateTime> func2 = (Book book) => book.PublicationDate;
+            Console.WriteLine($"Publication Date for Book 1 is : {func2(book1)}");
+
+            Func<Book, string> func3 = delegate (Book book) { return BookFunctions.GetTitle(book); };
+            Func<Book,string> func4 = delegate (Book book) { return BookFunctions.GetPrice(book); };
+            Func<Book,string> func5 = delegate (Book book) { return BookFunctions.GetAuthor(book); };
+            LibraryEngine.ProcessBooks(books, func3);
+            LibraryEngine.ProcessBooks(books, func4);
+            LibraryEngine.ProcessBooks(books, func5);
+
+        }
+    }
+}
